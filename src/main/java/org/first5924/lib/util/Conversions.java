77a5703edc0dc542d78f68dedf2221b893d1c6b8
Chris Voon <christopher.voon@gmail.com>
2022-08-15 23:42:12 -0700

    Revamp Conversions class

--- src/main/java/org/first5924/lib/util/Conversions.java
+++ src/main/java/org/first5924/lib/util/Conversions.java
@@ -5,27 +5,43 @@ import edu.wpi.first.math.util.Units;
 public class Conversions {
     private Conversions() {};
 
-    public static double robotMetersPerSecondToFalconUnitsPer100ms(double metersPerSecond, double wheelCircumferenceInches, double gearboxRatio) {
-        double metersPer100ms = metersPerSecond / 10;
-        double wheelRotationsPer100ms = metersPer100ms / Units.inchesToMeters(wheelCircumferenceInches);
-        double motorRotationsPer100ms = wheelRotationsPer100ms * gearboxRatio;
-        double falconUnitsPer100ms = motorRotationsPer100ms * 4096;
-        return falconUnitsPer100ms;
+    public static double robotMetersToSensorUnits(double robotMeters, double gearRatio, double wheelCircumferenceInches) {
+        double wheelRotations = robotMeters / Units.inchesToMeters(wheelCircumferenceInches);
+        double motorRotations = wheelRotations * gearRatio;
+        double sensorUnits = motorRotations * 2048;
+        return sensorUnits;
     }
 
-    public static double falconUnitsPer100msToRobotMetersPerSecond(double falconUnitsPer100ms, double wheelCircumferenceInches, double gearboxRatio) {
-        double motorRotationsPer100ms = falconUnitsPer100ms / 4096;
-        double wheelRotationsPer100ms = motorRotationsPer100ms / gearboxRatio;
-        double metersPer100ms = wheelRotationsPer100ms * Units.inchesToMeters(wheelCircumferenceInches);
-        double metersPerSecond = metersPer100ms * 10;
-        return metersPerSecond;
+    public static double sensorUnitsToRobotMeters(double sensorUnits, double gearRatio, double wheelCircumferenceInches) {
+        double motorRotations = sensorUnits / 2048;
+        double wheelRotations = motorRotations / gearRatio;
+        double robotMeters = wheelRotations * Units.inchesToMeters(wheelCircumferenceInches);
+        return robotMeters;
     }
 
-    public static double sensorUnitsToRobotMeters(double sensorUnits, double wheelCircumferenceInches) {
-        return sensorUnits * 4096 * Units.inchesToMeters(wheelCircumferenceInches);
+    public static double RPMToFalcon(double RPM, double gearRatio) {
+        double motorRPM = RPM * gearRatio;
+        double sensorUnitsPerMinute = motorRPM * 2048;
+        double falcon = sensorUnitsPerMinute / 600.0;
+        return falcon;
     }
 
-    public static double robotMetersToSensorUnits(double robotMeters, double wheelCircumferenceInches) {
-        return robotMeters / Units.inchesToMeters(wheelCircumferenceInches) / 4096;
+    public static double falconToRPM(double falcon, double gearRatio) {
+        double sensorUnitsPerMinute = falcon * 600;
+        double motorRPM = sensorUnitsPerMinute / 2048;
+        double mechRPM = motorRPM / gearRatio;
+        return mechRPM;
+    }
+
+    public static double MPSToFalcon(double MPS, double gearRatio, double wheelCircumferenceInches) {
+        double sensorUnitsPerSecond = robotMetersToSensorUnits(MPS, gearRatio, wheelCircumferenceInches);
+        double falcon = sensorUnitsPerSecond / 10;
+        return falcon;
+    }
+
+    public static double falconToMPS(double falcon, double gearRatio, double wheelCircumferenceInches) {
+        double sensorUnitsPerSecond = falcon * 10;
+        double MPS = sensorUnitsToRobotMeters(sensorUnitsPerSecond, gearRatio, wheelCircumferenceInches);
+        return MPS;
     }
 }
