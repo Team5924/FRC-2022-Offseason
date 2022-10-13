85f09004a670f2f90d114d3aa1f0d9614116728b
Chris Voon <christopher.voon@gmail.com>
2022-10-12 17:07:19 -0700

    Remove intake Stop command

--- src/main/java/org/first5924/frc2022/robot/RobotContainer.java
+++ src/main/java/org/first5924/frc2022/robot/RobotContainer.java
@@ -13,7 +13,6 @@ import org.first5924.frc2022.commands.intake.DeployIntake;
 import org.first5924.frc2022.commands.intake.Eject;
 import org.first5924.frc2022.commands.intake.RunIntake;
 import org.first5924.frc2022.commands.intake.RetractIntake;
-import org.first5924.frc2022.commands.intake.Stop;
 import org.first5924.frc2022.commands.autonomous.routines.DriveOneMeter;
 import org.first5924.frc2022.commands.autonomous.routines.FiveBallAuto;
 import org.first5924.frc2022.commands.autonomous.routines.FiveBallAutoPrint;
@@ -86,7 +85,6 @@ public class RobotContainer {
 
     mButtonA.whenPressed(new DeployIntake(mIntake));
     mButtonB.whenPressed(new RetractIntake(mIntake));
-    mButtonX.whenPressed(new Stop(mIntake));
     mButtonY.whenHeld(new Eject(mIntake));
   }
 
